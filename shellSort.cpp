0f31ba352afaa17f0d5a8508ba7672a9a77bdb0a
lyz <702368372@qq.com>
2015-08-26 11:34:45 +0800

    tab->space

--- shellSort.cpp
+++ shellSort.cpp
@@ -7,33 +7,33 @@ using namespace std;
  */
 void shellSort(int *arr, int size)
 {
-	//分组进行排序
-	for(int gap = size/2; gap > 0; gap /= 2)
-	{
-		//遍组中的数据，用插入法排序
-		for(int i = gap; i < size ; i++)
-		{
-			int ele = arr[i];
-			int j = 0;
-			//寻找插入点
-			for(j = i - gap; j >= 0 && ele < arr[j]; j -= gap)
-			{
-				arr[j+gap] = arr[j];
-			}
-			//插入
-			arr[j+gap] = ele;
-		}			
-	}
+  //分组进行排序
+  for(int gap = size/2; gap > 0; gap /= 2)
+  {
+    //遍组中的数据，用插入法排序
+    for(int i = gap; i < size ; i++)
+    {
+      int ele = arr[i];
+      int j = 0;
+      //寻找插入点
+      for(j = i - gap; j >= 0 && ele < arr[j]; j -= gap)
+      {
+        arr[j+gap] = arr[j];
+      }
+      //插入
+      arr[j+gap] = ele;
+    }
+  }
 }
 
 void main()
 {
-	int arr[] = {1, 3, 2, 6, 8, 9, 4, 7, 5, 0};
-	const int SIZE = 10;
-	shellSort(arr, SIZE);
-	for(int k = 0; k < SIZE; k++)
-	{
-		cout << arr[k] << ", ";
-	}
+  int arr[] = {1, 3, 2, 6, 8, 9, 4, 7, 5, 0};
+  const int SIZE = 10;
+  shellSort(arr, SIZE);
+  for(int k = 0; k < SIZE; k++)
+  {
+    cout << arr[k] << ", ";
+  }
 
 }
